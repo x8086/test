8a4f45353b21c8fd60ae27114232e89dae9be9e9
x8086 <47099666+x8086@users.noreply.github.com>
2019-07-12 19:37:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
-(function localFileVideoPlayerInit(win) {
-    var URL = win.URL || win.webkitURL,
-        displayMessage = (function displayMessageInit() {
-            var node = document.querySelector('#message');
+(function localFileVideoPlayer() {
+	'use strict'
+  var URL = window.URL || window.webkitURL
+  var displayMessage = function (message, isError) {
+    var element = document.querySelector('#message')
+    element.innerHTML = message
+    element.className = isError ? 'error' : 'info'
+  }
+  var playSelectedFile = function (event) {
+    var file = this.files[0]
+    var type = file.type
+    var videoNode = document.querySelector('video')
+    var canPlay = videoNode.canPlayType(type)
+    if (canPlay === '') canPlay = 'no'
+    var message = 'Can play type "' + type + '": ' + canPlay
+    var isError = canPlay === 'no'
+    displayMessage(message, isError)
 
-            return function displayMessage(message, isError) {
-                node.innerHTML = message;
-                node.className = isError ? 'error' : 'info';
-            };
-        }()),
-        playSelectedFile = function playSelectedFileInit(event) {
-            var file = this.files[0];
+    if (isError) {
+      return
+    }
 
-            var type = file.type;
-
-            var videoNode = document.querySelector('video');
-
-            var canPlay = videoNode.canPlayType(type);
-
-            canPlay = (canPlay === '' ? 'no' : canPlay);
-
-            var message = 'Can play type "' + type + '": ' + canPlay;
-
-            var isError = canPlay === 'no';
-
-            displayMessage(message, isError);
-
-            if (isError) {
-                return;
-            }
-
-            var fileURL = URL.createObjectURL(file);
-
-            videoNode.src = fileURL;
-        },
-        inputNode = document.querySelector('input');
-       
+    var fileURL = URL.createObjectURL(file)
+    videoNode.src = fileURL
+  }
+  var inputNode = document.querySelector('input')
+  inputNode.addEventListener('change', playSelectedFile, false)
+})()
